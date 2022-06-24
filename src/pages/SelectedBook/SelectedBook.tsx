0d50ace6afd9205fbfe07b1ab3a47adf1563a78f
ShamenkovIvan <shamenkov.i@routeamgroup.com>
2022-06-24 16:35:13 +0300

    add ErrorPage

--- src/pages/SelectedBook/SelectedBook.tsx
+++ src/pages/SelectedBook/SelectedBook.tsx
@@ -11,6 +11,7 @@ import { ISelectedBook } from '../../store/selectedBook/selectedBookType';
 // ui
 import { Spinner, CloseButton } from 'react-bootstrap';
 import cover from '../../assets/img/oblozhka.jpg' 
+import ErrorPage from '../ErrorPage';
 
 
 const SelectedBook = () => {
@@ -33,54 +34,59 @@ const SelectedBook = () => {
   } else {
     image = cover
   }
-
-  return (
-    <>
-    {
-      !bookState.isLoad ? <Spinner animation='border' variant='light'/> :
-      (
-      <div className='App__central books container col-10'>
-        <Link to="/">
-          <CloseButton className='close'/>
-        </Link>
-
-        <div className='selected-book selected-book__inner d-flex flex-column flex-lg-row align-items-center'>
-            <div className='col-lg-4 col-10 d-flex justify-content-center'>
-              <img
-              src={image}
-                alt='book cover'
-                className="selected-book__cover"></img>
-            </div>
-            <div className='selected-book__info col-10 col-lg-7'>
-              <span className='selected-info__categories'>
-                {book.categories}
-              </span>
-
-              <h2 className='selected-book__title selected-info__title'>
-                {book.title}
-              </h2>
-
-              <div className='selected-book__authors'>
-                {
-                  book.authors?.map((author:string, index:number) => {
-                    return (
-                        <span key={index}>
-                          {author}
-                          { index + 1 < book.authors.length ? ', ' : ''}
-                          </span>
-                    )
-                  })
-                }
+  
+  if (!bookState.isFalse) {
+    return (
+      <>
+      {
+        !bookState.isLoad ? <Spinner animation='border' variant='light'/> :
+        (
+        <div className='App__central books container col-10'>
+          <Link to="/">
+            <CloseButton className='close'/>
+          </Link>
+  
+          <div className='selected-book selected-book__inner d-flex flex-column flex-lg-row align-items-center'>
+              <div className='col-lg-4 col-10 d-flex justify-content-center'>
+                <img
+                src={image}
+                  alt='book cover'
+                  className="selected-book__cover"></img>
               </div>
-
-              <div className='selected-book__description'>{book.description}</div>
+              <div className='selected-book__info col-10 col-lg-7'>
+                <span className='selected-info__categories'>
+                  {book.categories}
+                </span>
+  
+                <h2 className='selected-book__title selected-info__title'>
+                  {book.title}
+                </h2>
+  
+                <div className='selected-book__authors'>
+                  {
+                    book.authors?.map((author:string, index:number) => {
+                      return (
+                          <span key={index}>
+                            {author}
+                            { index + 1 < book.authors.length ? ', ' : ''}
+                            </span>
+                      )
+                    })
+                  }
+                </div>
+  
+                <div className='selected-book__description'>{book.description}</div>
+            </div>
           </div>
         </div>
-      </div>
-      )
-    }
-    </>
+        )
+      }
+      </>
+    )
+  } else return (
+    <ErrorPage/>
   )
+  
 }
 
 export default SelectedBook
