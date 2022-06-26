06b9518c4b648beb7a7aea3b8daa0056055e0bcd
ShamenkovIvan <shamenkov.i@routeamgroup.com>
2022-06-26 21:34:17 +0300

    add proxy server

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,18 +1,12 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
-
 export interface IAxiosResponse {
   request<T = any, R = AxiosResponse<T>>(config: AxiosRequestConfig): Promise<R>
 }
 
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
+
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Expose-Headers': '*'
-  }
+  baseURL: PROXY_URL + process.env.REACT_APP_API_URL,
 })
 
 export { client }
